5468618affa75667add45ec291a2fbea4750112a
leonard475192 <leonard.t1028@gmail.com>
2024-03-23 23:15:12 +0900

    feat: adminとuser向けサイトのjsのわける

---
+++ workspaces/client/src/admin.tsx
@@ -0,0 +1,18 @@
+// import './side-effects';
+
+import $ from 'jquery';
+import ReactDOM from 'react-dom/client';
+
+import { AdminApp } from '@wsh-2024/admin/src/index';
+
+// import { preloadImages } from './utils/preloadImages';
+import { registerServiceWorker } from './utils/registerServiceWorker';
+
+const main = async () => {
+  await registerServiceWorker();
+  // await preloadImages();
+
+  ReactDOM.createRoot($('#root').get(0)!).render(<AdminApp />);
+};
+
+main().catch(console.error);
